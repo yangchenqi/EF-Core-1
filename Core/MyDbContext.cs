2a69256b73db56f1cbf8d8896ff37c3bec6a7c4c
ycq <1733692904@qq.com>
2025-10-30 14:51:55 +0800

    basic files

---
+++ Core/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_core1
+{
+     class MyDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=demo1;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //从当前程序集加载所有的
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EF_core1;
+using System.Linq;
+
+using (MyDbContext ctx = new MyDbContext()) { 
+    var book = new Book()
+    {
+        Title = "C#高级编程",
+        PubTime = DateTime.Now,
+        Price = "88.8"
+    };
+    //插入数据
+    ctx.Books.Add(book);//把book对象添加到逻辑上的Books表中
+
+    //查询数据  大部分linq语句都是可以被EF core转换成sql语句的
+    var books = ctx.Books.Where(b => b.Price == "88.8").ToList();
+    foreach (var b in books)
+    {
+        Console.WriteLine($"{b.Id} {b.Title} {b.PubTime} {b.Price}");
+    }
+
+    //修改数据  删除数据
+    var cb = ctx.Books.Where(b=>b.Title == "C#高级编程").FirstOrDefault();
+    cb.PubTime = DateTime.Now.AddDays(-10);
+    await ctx.SaveChangesAsync();
+
+    var d = ctx.Persons.Single(x=>x.Id == 1);//查询主键为1的Person对象
+    ctx.Persons.Remove(d);//删除这个对象
+
+    ctx.SaveChanges();//update-database  才算是真正的插入到数据库
+    //目前没有高效的批量插入方法，可以暂时使用Zack.EFCore.Batch Extensions这个第三方库 
+}
+
