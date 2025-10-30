bf3b6dfc6b60e22ed5a58633b26c7dfe37e514c0
ycq <1733692904@qq.com>
2025-10-30 14:58:09 +0800

    Reapply "basic files"
    This reverts commit ab43a0711aa18ab19e4f689a06b2b59abb989f36.

---
+++ Core/BookConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_core1
+{
+    internal class BookConfig:IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            //Fluent API 配置实体类映射到数据库表的信息
+            builder.ToTable("T_Books");
+        }
+    }
+}
